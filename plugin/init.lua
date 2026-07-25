ae41dc3e049b009ef0cefa9258f9342c939f974c
Jack Bacon <jack@itoj.dev>
2026-07-25 04:23:47 +0100

    Update require path for wezterm plugin

--- plugin/init.lua
+++ plugin/init.lua
@@ -55,8 +55,8 @@ end
 ---returns the name of the package, used when requiring modules
 ---@return string
 local function get_require_path()
-  local path = "httpssCssZssZsgithubsDscomsZsadriankarlensZsbarsDswezterm"
-  local path_trailing_slash = "httpssCssZssZsgithubsDscomsZsadriankarlensZsbarsDsweztermsZs"
+  local path = "httpssCssZssZsgithubsDscomsZsItsThatOneJack-DevsZsbarsDswezterm"
+  local path_trailing_slash = "httpssCssZssZsgithubsDscomsZsItsThatOneJack-DevsZsbarsDsweztermsZs"
   return directory_exists(path_trailing_slash) and path_trailing_slash or path
 end
 
